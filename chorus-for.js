b1a42124a4df3b57c6d998fd92daa028a3b0e38a
S550Stang <prabhuvardhanrao94@gmail.com>
2021-10-01 04:38:57 +0000

    Chorus With For Loops

---
+++ chorus-for.js
@@ -0,0 +1,11 @@
+const chorus = "lets dance";
+
+for (let i = 0; i < 10; i += 1) {
+  console.log(chorus);
+  if (i === 5) {
+    console.log("change the beat ");
+  } else if (i === 9) {
+    console.log("Atlast Turn Beat Up! And ");
+  }
+}
+console.log("Until the sun comes up!");
