64a2c8ba68c8027ee8031fbe96a67df1c4d56be3
S550Stang <prabhuvardhanrao94@gmail.com>
2021-10-08 16:56:49 +0000

    add for loop

--- packing-list.js
+++ packing-list.js
@@ -14,3 +14,12 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 console.log(packingList[3]);
+
+// using while loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
