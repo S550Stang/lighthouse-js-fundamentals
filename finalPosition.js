0bb4e4a5987d1b8adaba74508d1da4eaf99f2bd8
S550Stang <prabhuvardhanrao94@gmail.com>
2021-10-27 19:52:49 +0000

    new changes

--- finalPosition.js
+++ finalPosition.js
@@ -1,21 +1,23 @@
-const finalPosition = (moves) => {
-  var position = [0, 0];
-  for (var move of moves) {
+const moves = ["north", "north", "west", "west", "north", "east", "north"];
+
+const finalPosition = function (moves) {
+  let finalPosition = [0, 0]; //[(east,west),(north,south)]
+  for (const move of moves) {
     switch (move) {
       case "north":
-        position[1]++;
-        break;
-      case "south":
-        position[1]--;
+        finalPosition[1] += 1;
         break;
       case "east":
-        position[0]++;
+        finalPosition[0] += 1;
+        break;
+      case "south":
+        finalPosition[1] -= 1;
         break;
       case "west":
-        position[0]--;
+        finalPosition[0] -= 1;
         break;
     }
   }
-  return position;
-  a;
+  return finalPosition;
 };
+console.log(finalPosition(moves));
